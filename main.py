76aeb1c5958b074d206ea1d4ea50a3ba29f9aba5
Abena <priscillaabenaaboagye@gmail.com>
2025-01-23 17:10:42 +0000

    Working on the while loop

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ import random
 lowest_number = 1
 highest_number = 50
 answer = random.randint(lowest_number, highest_number)
+guesses = 0
+is_running = True
+
+print("Welcome to the Python Guessing game")
+print("------------------------------------")
+print(f"Select a number between {lowest_number} and {highest_number}")
+print("------------------------------------")
+
+
+while is_running:
+    guess = input("Select your number: ")
+
+    if not guess.isdigit():
+        print("Invalid")
+        print(f"Please select a number between {lowest_number} and {highest_number}")
+    else: 
+        pass
+
 
 # print(answer)
 
