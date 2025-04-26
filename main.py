f2033f4a383eb4a7c4071e12695a3f5bad9200ed
Abena <priscillaabenaaboagye@gmail.com>
2025-04-26 12:57:18 +0000

    Variable

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
-import random
+# Improving python skills using Bro Code
 
-lowest_number = 1
-highest_number = 50
-answer = random.randint(lowest_number, highest_number)
-print("python's answer = ", answer)
-guesses = 0
-is_running = True
+# Strings
+first_name = "Prscilla"
+food = "banku"
+print(f"Hello my name is {first_name}")
+print(f"My favourite food is {food}")
 
-print("Welcome to the Python Guessing game")
-print("------------------------------------")
-print(f"Select a number between {lowest_number} and {highest_number}")
-print("------------------------------------")
+# Integers
+age = 20
+quantity = 5
+num_of_students = 35
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"There are {num_of_students} people in the class")
 
+# float
+price = 4.5
+gpa = 3.5
+distance = 3.6
+print(f"The price of the goods are ${price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance} km")
 
-while is_running:
-    guess = input("Enter your guess: ")
+# boolean
+is_student = True
+for_sale = False
+is_online = True
 
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
 
-        if guess < lowest_number or guess > highest_number:
-            print("The number is out of range")
-            print(f"Please select a number between {lowest_number} and {highest_number}")
-        elif guess > answer:
-            print("Too High! Try again")
-        elif guess < answer:
-            print("Too Low! Try again")
-        else:
-            print(f"Correct! Tha answer was {answer}")
-            print(f"It took {guesses} guesses for you to get the amswer")
-            is_running = False
-    
-    else:
-        print("Invalid")
-        print(f"Please select a number between {lowest_number} and {highest_number}")
+if for_sale:
+    print("This is for sale")
+else:
+    print("This is NOT for sale")
 
+if is_online:
+    print("You are online")
+else:
+    print("You are NOT online")
 
+# Assignment
+name = "Priscilla"
+year = 2025
+amount = 3.7
+student = True
 
-    
-
-
-# print(answer)
 
