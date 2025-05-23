a422e58b7473ecdddb74e5c3f66394ca3b2781b1
Abena <priscillaabenaaboagye@gmail.com>
2025-05-23 13:18:22 +0000

    user input validator

--- tester.py
+++ tester.py
@@ -1,1 +1,12 @@
+def validates_user_input():
+    user_age = input("Enter your age: ")
+    if user_age.isdigit():
+        user_age = int(user_age)
+        if user_age >= 18:
+            print("You are eligible to vote")
+        elif user_age < 18:
+            print("You are not eligible to vote")
+    else: 
+        print("Invalid input. Please enter a number")
 
+validates_user_input() 
