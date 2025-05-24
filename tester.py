ec403026bd7f65a34a791940909de4694a3e0af9
Abena <priscillaabenaaboagye@gmail.com>
2025-05-24 10:30:21 +0000

    Q1, Q2

--- tester.py
+++ tester.py
@@ -10,3 +10,23 @@ def validates_user_input():
         print("Invalid input. Please enter a number")
 
 validates_user_input() 
+
+## Q1 
+def favourite_animal():
+    fav_ani = input("Enter your favourite animal: ")
+    if fav_ani.isdigit():
+        print("Please enter an animal")
+        input("Enter your favourite animal: ")
+    else:
+        print("I like too!")
+favourite_animal()
+
+## Q2
+def product_of_two_num():
+    first_num = int(input("Enter the first num: "))
+    second_num = int(input("Enter the second num: "))
+    product = first_num * second_num
+    print(f"The product of the numbers are {product}")
+product_of_two_num()
+
+# Q3
