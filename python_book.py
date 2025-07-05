45c98aa6a621f0922f17d3e08f6696507c8f73c0
Abena <priscillaabenaaboagye@gmail.com>
2025-07-05 17:48:09 +0000

    The print message

---
+++ python_book.py
@@ -0,0 +1,10 @@
+# Simple Message: Assign a message to a variable, and then print that message
+message = "Python here I come"
+print(message)
+
+# Assign a message to a variable, and print that message.
+#  Then change the value of the variable to a new message, and print the new message.
+message_2 = "The cat is a motivator"
+print(message_2)
+message_2 = "I have to finish this python book"
+print(message_2)
