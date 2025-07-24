92f729f56d2ead10292daf3805dcbbd62a04af3f
Abena <priscillaabenaaboagye@gmail.com>
2025-07-24 14:31:47 +0000

    test the module

---
+++ trials.py
@@ -0,0 +1,22 @@
+'''
+
+def user_name(name):
+	print(f"Hello!, {name}. Nice to meet you.")
+user_name("Alice")	
+
+def area_of_rectangle(length, width):
+	area = length * width
+	return area
+ans = area_of_rectangle(3, 4)
+print(ans)
+
+def check_status(number):
+	if number % 2 == 0:
+		print(f"{number} is an even number")
+	else:
+		print(f"{number} is an odd number")
+check_status(22)
+'''
+import math_function
+result = math_function.add(2, 3)
+print(result)
